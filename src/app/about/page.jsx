065fce9327e08d757a19aa61d4e65099d8229d6b
Miriam Rawlins <miriammrawlins@gmail.com>
2025-04-14 08:13:33 -0600

    breaking out stuff into components

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,20 @@
 export default function About() {
-    return (
-        <p>about page</p>
-    )
+  return (
+    <div className="p-5">
+      <h1 className="bg-slate-600 text-white p-3 rounded-t-lg">
+        ABOUT THIS GENERATOR
+      </h1>
+      <p className="text-xl font-serif p-4">
+        This is a D&D 5e encounter generator. You can create and save encounters
+        for your campaigns.
+      </p>
+      <p className="text-xl font-serif p-4">
+        This project was created as a personal project to help DMs generate
+        encounters quickly and easily.
+      </p>
+      <p className="text-xl font-serif p-4">
+        It was created using React and Next.js.
+      </p>
+    </div>
+  );
 }
